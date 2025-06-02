ec5de70d3099def198f9d5a32753ed501be396d0
CrisD404 <cristiandelcanto880@gmail.com>
2025-06-02 12:54:14 -0300

    chore: add linter and setup pre-commit with husky

--- src/components/common/types/patient.ts
+++ src/components/common/types/patient.ts
@@ -1,10 +1,9 @@
 export type Patient = {
-    id: number,
-    date_creation: Date,
-    name: string,
-    dni?: number,
-    diagnosis?: string,
-    observations?: string,
-    birthdate?: Date,
-}
-
+	id: number;
+	date_creation: Date;
+	name: string;
+	dni?: number;
+	diagnosis?: string;
+	observations?: string;
+	birthdate?: Date;
+};

8b6114c5dbdcbdeaa3b8e09269c4d53dc9601f4c
Solaris-99 <e.cifuentes996@gmail.com>
2025-06-02 14:56:37 -0300

    feat: reworked some components for patient view. Main rebase

--- src/components/patient/PatientView.tsx
+++ src/components/patient/PatientView.tsx
@@ -1,34 +1,42 @@
-import {
-	Box,
-	Button,
-	Container,
-	Link,
-	List,
-	ListItemText,
-	Paper,
-	Typography,
-} from '@mui/material';
-import type { Patient } from '../common/types/patient';
+import { Typography, Container, Box, List, ListItemButton, Button, Accordion, AccordionDetails, AccordionSummary } from "@mui/material"
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import type { Patient } from "../common/types/patient";
 
-export default function PatientView(props: { patients: Patient[] }) {
-	return (
-		<Container>
-			<Typography variant='h2'>Pacientes</Typography>
-			<Box>
-				<Button>Agregar paciente</Button>{' '}
-				{/* El botón debería ser solo para el coordinador */}
-				<Paper>
-					<List>
-						{props.patients.map((e) => {
-							return (
-								<ListItemText key={e.id}>
-									<Link href='#'>{e.name}</Link>
-								</ListItemText>
-							);
-						})}
-					</List>
-				</Paper>
-			</Box>
-		</Container>
-	);
+
+
+export default function PatientView(props: {patients : Patient[]}) {
+
+
+    return (
+    <Container>
+        <Accordion defaultExpanded>
+            <AccordionSummary
+            expandIcon={<ExpandMoreIcon/>}
+            >
+
+                <Typography variant="h5">Lista de pacientes</Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+
+                <Box display={"flex"} justifyContent={"space-between"} alignItems={"center"} width={"95%"} marginX="auto" >
+                    <Typography>{props.patients.length} Pacientes</Typography>
+                    <Button>Agregar paciente</Button> { /* El botón debería ser solo para el coordinador */ }
+                </Box>
+               
+                <List>
+                    {props.patients.map(e=>{
+                        return(
+                            <ListItemButton component="a" key={e.id}  href="#">
+                                    {e.name}
+                            </ListItemButton >)
+                    })}
+                </List>    
+            
+                
+            </AccordionDetails>
+
+        </Accordion>
+    </Container>
+    )
+
 }
